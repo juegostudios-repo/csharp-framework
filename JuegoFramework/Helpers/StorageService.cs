c92f9e83ecbcd704cd6dcd7ff702f21bbd75a7e7
Dishan M <dishan.m@juegostudioz.com>
2024-11-27 13:19:55 +0530

    Add: AWS S3 storage helper and configurable storage service

---
+++ JuegoFramework/Helpers/StorageService.cs
@@ -0,0 +1,34 @@
+namespace JuegoFramework.Helpers;
+
+public class StorageService
+{
+    public static string GetPresignedURL(string fileName, int expirySeconds)
+    {
+        if (Environment.GetEnvironmentVariable("USE_STORAGE_SYSTEM") == "AZURE")
+        {
+            return AzureBlobStorage.GetPresignedURL(fileName, expirySeconds);
+        }
+
+        if (Environment.GetEnvironmentVariable("USE_STORAGE_SYSTEM") == "AWS")
+        {
+            return AWSSimpleStorage.GetPresignedURL(fileName, expirySeconds);
+        }
+
+        throw new Exception("Please define USE_STORAGE_SYSTEM environment variable");
+    }
+
+    public static string GetUrl(string path)
+    {
+        if (Environment.GetEnvironmentVariable("USE_STORAGE_SYSTEM") == "AZURE")
+        {
+            return AzureBlobStorage.GetUrl(path);
+        }
+
+        if (Environment.GetEnvironmentVariable("USE_STORAGE_SYSTEM") == "AWS")
+        {
+            return AWSSimpleStorage.GetUrl(path);
+        }
+
+        throw new Exception("Please define USE_STORAGE_SYSTEM environment variable");
+    }
+}
