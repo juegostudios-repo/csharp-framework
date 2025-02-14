d1004602a3d89cfb08276826d0b876075d6be1bc
Flawid DSouza <flawid.d@juegostudioz.com>
2025-02-14 17:12:07 +0530

    template fixes

--- build-and-publish-template.sh
+++ build-and-publish-template.sh
@@ -1,6 +1,6 @@
-cd Templates
+cd JuegoFrameworkTemplates
 rm -f bin/Release/*.nupkg
-dotnet new uninstall JuegoFramework.Templates 
 dotnet pack
-dotnet new install bin/Release/*.nupkg 
-# dotnet nuget push "bin/Release/*.nupkg" --api-key $NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+dotnet nuget push "bin/Release/*.nupkg" --api-key $NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+# dotnet new uninstall JuegoFramework.Templates
+# dotnet new install bin/Release/*.nupkg
