4c903beda83c067d19a4c995e62ccfda923d1c61
Avinash-Kulal <92789991+Avinash-Kulal@users.noreply.github.com>
2025-02-28 16:29:45 +0530

    feat: add library template

---
+++ JuegoFrameworkTemplates/content/extensions/Library/Library/SqlLib/CustomLib.cs
@@ -0,0 +1,10 @@
+using ProjectName.Models;
+using JuegoFramework.Helpers;
+
+namespace ProjectName.Library
+{
+    public class CustomLib : MySqlLib<Custom>
+    {
+        // TODO: Write your logic here
+    }
+}
