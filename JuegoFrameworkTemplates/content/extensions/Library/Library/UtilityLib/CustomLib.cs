b4c5c28f73de93d530da732521127cada2e19fae
Avinash-Kulal <92789991+Avinash-Kulal@users.noreply.github.com>
2025-03-04 16:13:21 +0530

    New template for library (#6)

---
+++ JuegoFrameworkTemplates/content/extensions/Library/Library/UtilityLib/CustomLib.cs
@@ -0,0 +1,6 @@
+namespace ProjectName.Library;
+
+public class Custom
+{
+    // TODO: Write your logic here
+}
---
+++ JuegoFrameworkTemplates/content/extensions/Library/Models/Custom.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectName.Models
+{
+    [Table("custom")]
+    public class Custom
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("custom_id")]
+        public long CustomId { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("created_at", TypeName = "TIMESTAMP")]
+        public DateTime CreatedAt { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [Column("updated_at", TypeName = "TIMESTAMP")]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
